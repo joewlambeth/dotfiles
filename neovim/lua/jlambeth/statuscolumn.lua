1e9be1832ea977284da45345263542ac5707e8d2
jlambeth <joseph.w.lambeth1@aexp.com>
2025-11-10 21:14:19 -0500

    progress on statuscolumn

--- neovim/lua/jlambeth/statuscolumn.lua
+++ neovim/lua/jlambeth/statuscolumn.lua
@@ -1,16 +1,42 @@
 -- https://www.reddit.com/r/neovim/comments/1djjc6q/statuscolumn_a_beginers_guide/
+-- https://github.com/akinsho/dotfiles/blob/main/.config/nvim/plugin/statuscolumn.lua#L56-L72
 
 _G.MY_STATUS = function()
 	local screen_height = math.max(2, #tostring(vim.api.nvim_win_get_height(0)))
 	local buffer_height = math.max(3, #tostring(vim.api.nvim_buf_line_count(0)))
 	local current_win_bufnr = vim.api.nvim_get_current_buf()
-	local active_bufnr = vim.fn.bufnr(0)
+
+	local signs = vim.api.nvim_buf_get_extmarks(
+		current_win_bufnr,
+		-1,
+		{ vim.v.lnum - 1, 0 },
+		{ vim.v.lnum - 1, -1 },
+		{ details = true, type = "sign" }
+	)
+
+	local git_border = nil
+	local winner, priority = nil, 99999
+	for _, v in pairs(signs) do
+		local item = v[4]
+
+		if item.sign_hl_group:match("^Git") then
+			git_border = "%#" .. item.sign_hl_group .. "#" .. item.sign_text, 1, 2
+		elseif priority >= item.priority then
+			winner = "%#" .. item.sign_hl_group .. "#" .. string.sub(item.sign_text, 1, 1) .. "%#Normal#"
+			priority = item.priority
+		end
+	end
 
 	local drawn_win = vim.api.nvim_get_current_win()
 	local focused = tonumber(vim.g.actual_curwin) == tonumber(drawn_win)
 
+	if git_border ~= nil and winner == nil then
+		vim.print("BAD! " .. git_border)
+	end
+
 	local border = function()
-		return "│"
+		return git_border or "│"
+		-- return "│"
 	end
 
 	local show_wrap = function(var, repl)
@@ -38,11 +64,12 @@ _G.MY_STATUS = function()
 	end
 
 	local text = table.concat({
+		winner or "  ",
 		focused and " " or "",
 		rnu(),
 		lnum(),
 		border(),
-	}, " ")
+	}, "")
 	return text
 end
 
